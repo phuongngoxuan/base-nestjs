f5f32903d21f5b63839ab3424eebd1763aff2d3d
phuongktvt <phuong.ngo@sotatek.com>
2022-08-12 12:10:12 +0700

    add handler event and alert

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module, Logger } from '@nestjs/common';
-import { AppGateway } from './modules/socket/app.gateway';
+import { EventGateway } from './modules/socket/app.event.gateway';
+import { AlertGateway } from './modules/socket/app.alert.gateway';
 import Modules from 'src/modules';
 @Module({
   imports: [...Modules],
   controllers: [],
-  providers: [Logger, AppGateway],
+  providers: [Logger, EventGateway, AlertGateway],
 })
 export class AppModule {}
---
+++ src/modules/socket/app.event.gateway.ts
@@ -0,0 +1,45 @@
+import {
+  ConnectedSocket,
+  MessageBody,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  WsResponse,
+} from '@nestjs/websockets';
+
+import { Logger } from '@nestjs/common';
+import { Server, Socket } from 'socket.io';
+
+@WebSocketGateway({
+  cors: {
+    origin: '*',
+  },
+  path: '/websocket',
+  serveClient: false,
+})
+export class EventGateway implements OnGatewayInit {
+  @WebSocketServer() wss: Server;
+
+  private logger: Logger = new Logger('AppGateway');
+  afterInit(server: Server) {
+    this.logger.log('init');
+  }
+
+  @SubscribeMessage('eventToServer')
+  handlerEvent(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    this.wss.emit('eventToClient', data);
+  }
+
+  @SubscribeMessage('alertToServer')
+  handlerAlert(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
+    console.log('in handlert alert server');
+    this.wss.emit('alertToClient', data);
+  }
+}

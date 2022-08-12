6262d901fbb71c156f7eb7c4ff214d5aa7deb4da
phuongktvt <phuong.ngo@sotatek.com>
2022-08-12 13:49:16 +0700

    feat namespace

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module, Logger } from '@nestjs/common';
-import { EventGateway } from './modules/socket/app.event.gateway';
-import { AlertGateway } from './modules/socket/app.alert.gateway';
+import { EventGateway } from './modules/socket/event.gateway';
+import { AlertGateway } from './modules/socket/alert.gateway';
 import Modules from 'src/modules';
 @Module({
   imports: [...Modules],
